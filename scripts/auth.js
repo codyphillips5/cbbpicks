82a047cffb9fce629b8eeec38ee70e73a0798d34
codyphillips5 <codyphill5@gmail.com>
2019-12-12 01:37:04 -0500

    cyhawk

--- scripts/auth.js
+++ scripts/auth.js
@@ -22,12 +22,9 @@ if(createForm) {
     createForm.addEventListener('submit', (e) => {
         e.preventDefault();
     
-        db.collection('week6_day1').add({
+        db.collection('week6_day2').add({
             user: auth.currentUser.email,
-            game51: document.getElementById('seasongame51').value,
-            game52: document.getElementById('seasongame52').value,
-            game53: document.getElementById('seasongame53').value,
-            game54: document.getElementById('seasongame54').value
+            game55: document.getElementById('seasongame55').value
         }).then(() => {
             // close the modal and reset form
             //const modal = document.querySelector('#modal-create');
