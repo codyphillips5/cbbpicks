7d461f2d92b6857d4558d1e2c7140c0b0ca3ddf7
codyphillips5 <codyphill5@gmail.com>
2019-12-29 09:00:24 -0600

    sundee

--- scripts/auth.js
+++ scripts/auth.js
@@ -24,11 +24,11 @@ if(createForm) {
     
         db.collection('week8_day4').add({
             user: auth.currentUser.email,
-            game73: document.getElementById('seasongame76').value,
-            game74: document.getElementById('seasongame77').value,
-            game75: document.getElementById('seasongame78').value,
-            game75: document.getElementById('seasongame79').value,
-            game75: document.getElementById('seasongame80').value
+            game76: document.getElementById('seasongame76').value,
+            game77: document.getElementById('seasongame77').value,
+            game78: document.getElementById('seasongame78').value,
+            game79: document.getElementById('seasongame79').value,
+            game80: document.getElementById('seasongame80').value
         }).then(() => {
             // close the modal and reset form
             //const modal = document.querySelector('#modal-create');
