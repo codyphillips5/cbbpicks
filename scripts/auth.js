6cfeea3c9e9285112a9ac48f4f5b04ac2f755f31
codyphillips5 <codyphill5@gmail.com>
2020-12-29 23:22:32 -0500

    wed

--- scripts/auth.js
+++ scripts/auth.js
@@ -35,9 +35,8 @@ if(createForm) {
 	week.get()
 		.then((docSnapshot) => {
 			if (docSnapshot.data()) {
-				if (docSnapshot.data().game1 !== undefined) { 
-					document.getElementById("label-choice-seasongame51").innerHTML = `<label class="choice">${docSnapshot.data().game1} <span class="glyphicon glyphicon-plusglyphicon glyphicon-check"></span></label>`
-					document.getElementById("label-choice-seasongame52").innerHTML = `<label class="choice">${docSnapshot.data().game2} <span class="glyphicon glyphicon-plusglyphicon glyphicon-check"></span></label>`
+				if (docSnapshot.data().game3 !== undefined) { 
+					document.getElementById("label-choice-seasongame53").innerHTML = `<label class="choice">${docSnapshot.data().game3} <span class="glyphicon glyphicon-plusglyphicon glyphicon-check"></span></label>`
 					//console.log("Document data:", docSnapshot.data())
 				}
 			}
@@ -56,8 +55,7 @@ if(createForm) {
 			if (docSnapshot.exists) {
 				week.update({
 					user: auth.currentUser.email,
-					game1: document.getElementById('seasongame51').value,
-					game2: document.getElementById('seasongame52').value
+					game3: document.getElementById('seasongame53').value
 				}).then(function() {
 					success();
 				}).catch(err => {
@@ -67,8 +65,7 @@ if(createForm) {
 			} else {
 				week.set({
 					user: auth.currentUser.email,
-					game1: document.getElementById('seasongame51').value,
-					game2: document.getElementById('seasongame52').value
+					game3: document.getElementById('seasongame53').value
 				}).then(() => {
 					success();
 				}).catch(err => {
