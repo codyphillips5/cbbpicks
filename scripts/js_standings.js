f30887264ba81b23d12748be72b8a1307cdb5089
codyphillips5 <codyphill5@gmail.com>
2021-01-31 14:36:41 -0500

    tenet

--- scripts/js_standings.js
+++ scripts/js_standings.js
@@ -14,7 +14,7 @@ var getUsers= $.getJSON("https://codyphillips5.github.io/cbbpicks/json/users.jso
 		usersList = json;
 });
 
-var week = 9;
+var week = 10;
 for (var y = 1; y <= week; y++) {
 	weekList = weekList + `<th scope="col">Week ${y}</th>`;
 }
