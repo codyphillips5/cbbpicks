aec2e1674eca5fe1eab2231aaeff44fda749cc49
codyphillips5 <codyphill5@gmail.com>
2021-02-15 22:39:31 -0500

    tuez

--- scripts/auth.js
+++ scripts/auth.js
@@ -35,8 +35,8 @@ if(createForm) {
 	week.get()
 		.then((docSnapshot) => {
 			if (docSnapshot.data()) {
-				if (docSnapshot.data().game1 !== undefined) { 
-					document.getElementById("label-choice-seasongame121").innerHTML = `<label class="choice">${docSnapshot.data().game1} <span class="glyphicon glyphicon-plusglyphicon glyphicon-check"></span></label>`
+				if (docSnapshot.data().game2 !== undefined) { 
+					document.getElementById("label-choice-seasongame122").innerHTML = `<label class="choice">${docSnapshot.data().game2} <span class="glyphicon glyphicon-plusglyphicon glyphicon-check"></span></label>`
 					//console.log("Document data:", docSnapshot.data())
 				}
 			}
@@ -55,7 +55,7 @@ if(createForm) {
 			if (docSnapshot.exists) {
 				week.update({
 					user: auth.currentUser.email,
-					game1: document.getElementById('seasongame121').value
+					game2: document.getElementById('seasongame122').value
 				}).then(function() {
 					success();
 				}).catch(err => {
@@ -65,7 +65,7 @@ if(createForm) {
 			} else {
 				week.set({
 					user: auth.currentUser.email,
-					game1: document.getElementById('seasongame121').value
+					game2: document.getElementById('seasongame122').value
 				}).then(() => {
 					success();
 				}).catch(err => {
