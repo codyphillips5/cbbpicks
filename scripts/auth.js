04ecd800674b9a4353a8d769899f98456a9ffd83
codyphillips5 <codyphill5@gmail.com>
2021-11-13 01:42:12 -0500

    satty

--- scripts/auth.js
+++ scripts/auth.js
@@ -43,8 +43,8 @@ var activeScript = $.getScript("https://codyphillips5.github.io/cbbpicks/scripts
    console.log(arrayActive);
    startArray = arrayActive[0];
    lengthArray = arrayActive.length;
-   startArray = 4;
-   lengthArray = 4;
+   startArray = 8;
+   lengthArray = 2;
    console.log(startArray);
    console.log(lengthArray);
 });
