b6f8224e2b65f769ce4985d4cc996ebb6f9e4114
codyphillips5 <codyphill5@gmail.com>
2021-11-15 04:22:39 -0500

    juan

--- scripts/auth.js
+++ scripts/auth.js
@@ -21,7 +21,7 @@ auth.onAuthStateChanged(user => {
 var startArray = 0;
 var lengthArray = 0;
 var coversTeam = [];
-var weekNum = 1;
+var weekNum = 2;
 var empty = false;
 var firstName;
 var users = $.getJSON("https://codyphillips5.github.io/cbbpicks/json/users.json", function(json){
@@ -43,8 +43,8 @@ var activeScript = $.getScript("https://codyphillips5.github.io/cbbpicks/scripts
    console.log(arrayActive);
    startArray = arrayActive[0];
    lengthArray = arrayActive.length;
-   startArray = 10;
-   lengthArray = 1;
+   startArray = 1;
+   lengthArray = 3;
    console.log(startArray);
    console.log(lengthArray);
 });
--- scripts/js_picks.js
+++ scripts/js_picks.js
@@ -32,7 +32,7 @@ var gog = "";
 var coversTeam = [];
   
 // week of year, first game
-var week = 1;
+var week = 2;
 if (week === 0) {
 	document.getElementById("week-title").innerHTML = `<h2>🏀 March Madness</h2>`;
 }
