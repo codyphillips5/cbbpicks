bf414f36e8c036972b7d68299de6071fa58b2979
codyphillips5 <codyphill5@gmail.com>
2021-11-21 01:31:23 -0500

    the sun

--- scripts/auth.js
+++ scripts/auth.js
@@ -43,8 +43,8 @@ var activeScript = $.getScript("https://codyphillips5.github.io/cbbpicks/scripts
    console.log(arrayActive);
    startArray = arrayActive[0];
    lengthArray = arrayActive.length;
-   startArray = 6;
-   lengthArray = 2;
+   startArray = 8;
+   lengthArray = 3;
    console.log(startArray);
    console.log(lengthArray);
 });
