1dbd1c6a96405698b13c649d56fd4bb1c42fcd43
codyphillips5 <codyphill5@gmail.com>
2021-12-29 01:28:00 -0500

    updeights

--- scripts/auth.js
+++ scripts/auth.js
@@ -43,8 +43,8 @@ var activeScript = $.getScript("https://codyphillips5.github.io/cbbpicks/scripts
    console.log(arrayActive);
    startArray = arrayActive[0];
    lengthArray = arrayActive.length;
-   startArray = 10;
-   lengthArray = 1;
+   startArray = 1;
+   lengthArray = 3;
    console.log(startArray);
    console.log(lengthArray);
 });
