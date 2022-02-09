a3ad407edf34adc05a476d864e3fef5d7f8b58fd
codyphillips5 <codyphill5@gmail.com>
2022-02-08 22:47:19 -0600

    wedy van halen

--- scripts/auth.js
+++ scripts/auth.js
@@ -43,8 +43,8 @@ var activeScript = $.getScript("https://codyphillips5.github.io/cbbpicks/scripts
    console.log(arrayActive);
    startArray = arrayActive[0];
    lengthArray = arrayActive.length;
-   startArray = 1;
-   lengthArray = 2;
+   startArray = 3;
+   lengthArray = 1;
    console.log(startArray);
    console.log(lengthArray);
 });
