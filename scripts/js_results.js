31164e6e5dbcfd7a1e7e28b6a0e21e46771bab18
codyphillips5 <codyphill5@gmail.com>
2022-11-14 18:10:09 -0500

    rusty

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -6,7 +6,7 @@ var coversResultsTeam = [];
 var picksList, teamsList, resultsList, usersList;
 var badge = document.createElement('div');
 badge.className = 'standings';
-var weekNum = 1;
+var weekNum = 2;
 var weekList = "";
 var check = "";
 
