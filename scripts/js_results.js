b9b9877cd65a1788a0b93693e7d8f9d41253b4e6
Cody Phillips <codyphill5@gmail.com>
2023-01-09 00:45:52 -0500

    tenner

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -6,7 +6,7 @@ var coversResultsTeam = [];
 var picksList, teamsList, resultsList, usersList;
 var badge = document.createElement('div');
 badge.className = 'standings';
-var weekNum = 9;
+var weekNum = 10;
 var weekList = "";
 var check = "";
 
