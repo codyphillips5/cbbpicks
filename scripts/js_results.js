b7bb401120ef90db437d84f73ba6950e129dc6d9
codyp <cody.phillips@fox.com>
2023-12-27 11:10:59 -0500

    eight

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -6,7 +6,7 @@ var coversResultsTeam = [];
 var picksList, teamsList, resultsList, usersList;
 var badge = document.createElement('div');
 badge.className = 'standings';
-var weekNum = 7;
+var weekNum = 8;
 var weekList = "";
 var check = "";
 
