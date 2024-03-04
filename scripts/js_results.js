7301a05cdb199509c476d1eda02dad260d2232f5
codyp <cody.phillips@fox.com>
2024-03-04 10:02:28 -0500

    peyt

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -6,7 +6,7 @@ var coversResultsTeam = [];
 var picksList, teamsList, resultsList, usersList;
 var badge = document.createElement('div');
 badge.className = 'standings';
-var weekNum = 17;
+var weekNum = 18;
 var weekList = "";
 var check = "";
 
