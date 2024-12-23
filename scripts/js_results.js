266ddbef0ec18880202b01b110dd3fe01f5d7340
codyp <cody.phillips@fox.com>
2024-12-23 09:27:57 -0500

    mon

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -18,7 +18,7 @@ var storeUsers = [];
 var coversNum = [];
 var userPickTeams = [];
 var coversArr = [""];
-var weekNum = 7;
+var weekNum = 8;
 var weekList = "";
 var weeklyList = "";
 var isCorrect;
