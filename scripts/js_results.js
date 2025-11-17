9373550c5b3a61880869828bffb625c43515bad6
unknown <cody.phillips@fox.com>
2025-11-17 11:08:29 -0500

    three

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -18,7 +18,7 @@ var storeUsers = [];
 var coversNum = [];
 var userPickTeams = [];
 var coversArr = [""];
-var weekNum = 2;
+var weekNum = 3;
 var weekList = "";
 var weeklyList = "";
 var isCorrect;
