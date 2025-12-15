1441e0c9d4768e7b026048c4fa2d98d6ecb67312
unknown <cody.phillips@fox.com>
2025-12-15 10:40:00 -0500

    seven

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -18,7 +18,7 @@ var storeUsers = [];
 var coversNum = [];
 var userPickTeams = [];
 var coversArr = [""];
-var weekNum = 6;
+var weekNum = 7;
 var weekList = "";
 var weeklyList = "";
 var isCorrect;
