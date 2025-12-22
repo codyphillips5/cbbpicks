50c77d369ac1972982e393081d2c1f29efb4e769
unknown <cody.phillips@fox.com>
2025-12-22 02:04:06 -0500

    christmas

--- scripts/js_total.js
+++ scripts/js_total.js
@@ -20,7 +20,7 @@ var fullTeamImage = "";
 
 var standings, teams, resultsList, usersList;
 game = 0;
-totalWeek = 7;
+totalWeek = 8;
 
 for (var i = 1; i <= totalWeek; i++) {
 	var getGames = $.getJSON("https://codyphillips5.github.io/cbbpicks/json/games/week" + i + ".json", function(json){
