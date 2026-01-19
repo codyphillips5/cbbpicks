2172dce959d738ff88b0c4668698516a76794b9a
codyphillips5 <codyphill5@gmail.com>
2026-01-19 02:26:36 -0500

    twel

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -18,7 +18,7 @@ var storeUsers = [];
 var coversNum = [];
 var userPickTeams = [];
 var coversArr = [""];
-var weekNum = 11;
+var weekNum = 12;
 var weekList = "";
 var weeklyList = "";
 var isCorrect;
